8b91778095dcb462121a53074486a8597d959c90
Anthony <anthony.villegas@nodel.co>
2023-05-31 12:44:33 -0500

    test intercept

--- extensions/ui-fields-for-lineup/src/index.jsx
+++ extensions/ui-fields-for-lineup/src/index.jsx
@@ -9,11 +9,12 @@ import {
   Checkbox,
   Button,
   useExtensionCapability,
-  useTotalAmount,
   useApplyAttributeChange,
   useAttributes,
   useCartLines,
   Spinner,
+  useBuyerJourneyIntercept,
+  useBuyerJourneyCompleted,
 } from "@shopify/checkout-ui-extensions-react";
 
 render("Checkout::Dynamic::Render", () => <App />);
@@ -30,17 +31,73 @@ function App() {
   const canBlockProgress = useExtensionCapability("block_progress");
   const applyAttributeChange = useApplyAttributeChange();
   const attributes = useAttributes();
-  // const totalAmount = useTotalAmount();
-  const cartLines = useCartLines()
+  const cartLines = useCartLines();
+  const journey = useBuyerJourneyCompleted();
 
   console.log("attributes", attributes);
-  
+  console.log("journey", useBuyerJourneyCompleted());
 
   // get the sum of all values into cartLines array in cartLines[].cost.totalAmount.amount
   const subtotal = cartLines.reduce((acc, item) => {
     return acc + item.cost.totalAmount.amount;
   }, 0);
 
+  useBuyerJourneyIntercept(({ canBlockProgress }) => {
+    if (journey) {
+      return apiCall()
+        .then((result) => {
+          if (attributes.lineupCardValue.value !== "null") {
+            result.saldo += 4;
+            if (result.saldo !== saldoDisponible) {
+              const saldo =
+                result.saldo >= subtotal
+                  ? subtotal.toString()
+                  : result.saldo.toString();
+              setSaldoDisponible(data.saldo);
+              setSaldoGastar(saldo);
+              setCardAdded(true);
+              clearValidationErrors();
+              return {
+                behavior: "block",
+                reason: "La Tarjeta Lineup Rewards ha actualizado su saldo",
+                errors: [
+                  {
+                    // In addition, show an error at the page level
+                    message:
+                      "El saldo de su tarjeta Lineup Rewards ha cambiado",
+                  },
+                ],
+              };
+            } else {
+              return {
+                behavior: "allow",
+              };
+            }
+          } else {
+            return {
+              behavior: "allow",
+            };
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          return {
+            behavior: "block",
+            reason: "Existe un problema con la transacción",
+            errors: [
+              {
+                // In addition, show an error at the page level
+                message: "Existe un problema con la transacción",
+              },
+            ],
+          };
+        });
+    } else {
+      return {
+        behavior: "allow",
+      };
+    }
+  });
 
   useEffect(async () => {
     setLineupCardNumber("");
@@ -58,8 +115,9 @@ function App() {
     setValidationError("");
   }
 
-  function handleButtonClick() {
+  async function handleButtonClick() {
     if (useLineupCard) {
+      //Start validations
       const regex = /^[0-9]+$/;
 
       if (lineupCardNumber.trim() === "") {
@@ -79,37 +137,37 @@ function App() {
 
       setIsLoading(true); // Start loader
 
-      // Simulating fetch delay
-      setTimeout(async () => {
-        const data = {
-          esValido: true,
-          saldo: 2000,
-        };
+      const data = await apiCall(); // Call API
 
-        setIsLoading(false); // Stop loader
+      setIsLoading(false); // Stop loader
 
-        if (data.esValido) {
-          const saldo =
-            data.saldo >= subtotal
-              ? subtotal.toString()
-              : data.saldo.toString();
-          setSaldoDisponible(data.saldo);
-          setSaldoGastar(saldo);
-          setCardAdded(true);
-          clearValidationErrors();
-          //Agrega un el numero y valor de la tarjeta a los atributos
-          const change = await applyAttributeChange({
-            key: "lineupCardValue",
-            type: "updateAttribute",
-            value: saldo,
-          });
-        } else {
-          setValidationError(
-            "El número de la tarjeta Lineup Rewards es inválido"
-          );
-        }
-      }, 2000); // Replace this with your actual fetch call
+      if (data.esValido) {
+        const saldo =
+          data.saldo >= subtotal ? subtotal.toString() : data.saldo.toString();
+        setSaldoDisponible(data.saldo);
+        setSaldoGastar(saldo);
+        setCardAdded(true);
+        clearValidationErrors();
+        //Agrega un el numero y valor de la tarjeta a los atributos
+        await applyAttributeChange({
+          key: "lineupCardValue",
+          type: "updateAttribute",
+          value: saldo,
+        });
+      } else {
+        setValidationError(
+          "El número de la tarjeta Lineup Rewards es inválido"
+        );
+      }
     }
+  }
+
+  async function apiCall() {
+    // Simulating fetch delay
+    return {
+      esValido: true,
+      saldo: 2000,
+    };
   }
 
   async function handleRemoveCard() {
@@ -127,17 +185,16 @@ function App() {
     <BlockStack>
       <View padding="base">
         <Banner status="info">
-        <Checkbox
-          checked={useLineupCard}
-          onChange={setUseLineupCard}
-          disabled={isLoading ? true : false}
-        >
-          {useLineupCard
-            ? "Eliminar tarjeta Lineup Rewards"
-            : "Usar tarjeta Lineup Rewards para obtener un descuento"}
-        </Checkbox>
+          <Checkbox
+            checked={useLineupCard}
+            onChange={setUseLineupCard}
+            disabled={isLoading ? true : false}
+          >
+            {useLineupCard
+              ? "Eliminar tarjeta Lineup Rewards"
+              : "Usar tarjeta Lineup Rewards para obtener un descuento"}
+          </Checkbox>
         </Banner>
-        
       </View>
       {useLineupCard && (
         <BlockStack spacing="base">

542dbf19aacbbdd3165aa4069629272493681690
Anthony <anthony.villegas@nodel.co>
2023-06-07 13:42:18 -0500

    update de UI

--- extensions/ui-fields-for-lineup/src/index.jsx
+++ extensions/ui-fields-for-lineup/src/index.jsx
@@ -21,6 +21,13 @@ import {
 
 render("Checkout::Dynamic::Render", () => <App />);
 
+const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://b258-2800-bf0-8014-9d-81e4-b6f-32f6-8124.ngrok-free.app";
+const MAIN_TOKEN =
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjcmVkZW50aWFsX2lkIjoiYThlNThhMzAtODRlZi00YjMyLWFkN2MtYzcwYWZjMDkyMzBkIiwiZXhwIjoxNjg2NjY4MDI1LCJvcmlnX2lhdCI6MTY4NjA2MzIyNSwiaXNzIjoibG9jYWxob3N0LmNvbSJ9.XlRK03A6cjWzC2METd1oR-MjsXrJ5Q065LtnITVtGpM";
+const API_KEY = "9b2c299397f0cb50d8512e6067e18e1adb33a733";
+const API_SECRET = "^4-p8ijc^2sg9nh41&e$-oorj@66z@qs7f_qeofat=fkmn$hb3";
+
 function App() {
   // Set up the checkbox state
   const [useLineupCard, setUseLineupCard] = useState(false);
@@ -32,8 +39,9 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [isCardValueUpdated, setIsCardValueUpdated] = useState(false);
   const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [token, setToken] = useState(MAIN_TOKEN);
 
-  // Ref para almacenar el valor mutable de isTimerRunning
+  // is timer reference
   const isTimerRunningRef = useRef(isTimerRunning);
   isTimerRunningRef.current = isTimerRunning;
 
@@ -146,6 +154,7 @@ function App() {
 
       // Simulate an API call
       const data = await apiCall();
+      console.log(data);
 
       // Remove the loading spinner
       setIsLoading(false);
@@ -189,26 +198,90 @@ function App() {
     }
   }
 
-  // Simulate an API call
   async function apiCall() {
-    return {
-      esValido: true,
-      saldo: 2000,
+    //100799072
+
+    //Headers
+    var myHeaders = new Headers();
+    myHeaders.append("X-Requested-With", "");
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", `Bearer ${token}`);
+
+    //Body
+    var raw = JSON.stringify({
+      tarjeta: {
+        numero_de_tarjeta: lineupCardNumber,
+      },
+    });
+
+    //options
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
     };
+
+    //Call
+    try {
+      const response = await fetch(
+        API_URL + "/api/cliente/obtener-balance-easy/",
+        requestOptions
+      );
+
+      // Si el status es 401, obtenemos un nuevo token y reintentamos la llamada
+      if (response.status === 401) {
+        console.log("El token ha expirado, obteniendo uno nuevo...");
+        const newToken = await getNewToken();
+
+        // Actualizamos el token en las opciones de la solicitud
+        requestOptions.headers.set("Authorization", `Bearer ${newToken}`);
+
+        // Intentamos la llamada nuevamente
+        const response = await fetch(apiUrl, requestOptions);
+
+        // Si el status sigue siendo 401, lanzamos un error
+        if (response.status === 401) {
+          throw new Error("Error de autenticación después de renovar el token");
+        }
+      }
+
+      if (response.status === 404) {
+        return {
+          esValido: false,
+          saldo: 0,
+        };
+      }
+
+      if (response.status === 400) {
+        return {
+          esValido: false,
+          saldo: 0,
+        };
+      }
+
+      const responseData = await response.json();
+      return {
+        esValido: true,
+        saldo: responseData.credito,
+        extra: responseData,
+      };
+    } catch (error) {
+      // Manejo de errores de la llamada fetch
+      console.error("Error en la llamada API:", error);
+      throw error;
+    }
   }
 
   // Function to start the timer loop
   async function startTimerLoop(saldo) {
     let previousSaldo = saldo;
 
-    //testing
-    loop = 0;
-
     // Start the timer loop
     while (isTimerRunningRef.current) {
       // Wait for 5 seconds / 30Seconds
-      // await sleep(30000);
-      await sleep(1000);
+      await sleep(30000);
+      // await sleep(1000);
 
       let updatedData = await apiCall();
       let updatedSaldoGastar =
@@ -217,14 +290,6 @@ function App() {
           : updatedData.saldo.toString();
 
       //testing
-      console.log("loop", loop);
-      if (loop === 3) {
-        updatedData.saldo = 300;
-        updatedSaldoGastar =
-          updatedData.saldo >= subtotal
-            ? subtotal.toString()
-            : updatedData.saldo.toString();
-      }
 
       console.log("previousSaldo", previousSaldo);
       console.log("saldoGastar", updatedData.saldo);
@@ -264,6 +329,47 @@ function App() {
   // function to handle the remove card button
   function handleAccept() {
     setIsCardValueUpdated(false);
+  }
+
+  async function getNewToken() {
+    //Headers
+    var myHeaders = new Headers();
+    myHeaders.append("X-Requested-With", "");
+    myHeaders.append("Content-Type", "application/json");
+
+    //Body
+    var raw = JSON.stringify({
+      api_key: API_KEY,
+      api_secret: API_SECRET,
+    });
+
+    //options
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    //Call
+    try {
+      const response = await fetch(
+        API_URL + "/api/auth/access_token/",
+        requestOptions
+      );
+
+      // Si el status sigue siendo 401, lanzamos un error
+      if (response.status === 401) {
+        throw new Error("Error de autenticación después de renovar el token");
+      }
+      const new_token = response.json().token;
+      setToken(new_token);
+      return new_token;
+    } catch (error) {
+      // Manejo de errores de la llamada fetch
+      console.error("Error en la llamada API:", error);
+      throw error;
+    }
   }
 
   // Return the JSX to render
